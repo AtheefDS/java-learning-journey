617d2d56ce2c0f769183218ce90245ffc515b48f
Mohamed Atheef <atheefcse@gmail.com>
2026-06-21 23:32:25 +0530

    Array and for loop

---
+++ array.java
@@ -0,0 +1,26 @@
+//How to get integer input in arrays and also using for loop
+import java.lang.System;
+import java.util.Scanner;
+public class array {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //get array size from user 
+        System.out.println("Enter your array size:");
+        int n = scan.nextInt();
+        //create array
+        int[] arr = new int[n];
+        System.out.println("Enter " + n  + " elements:");
+        //using for loop for easy for get input
+        for(int i =0; i<n ; i++){
+            arr[i] = scan.nextInt();
+        }
+        //print using for loop printing
+        for(int i=0 ; i<n ; i++){
+            System.out.println("Position at "+ i + " : " + arr[i]);
+        }
+
+
+
+    }
+}
+
