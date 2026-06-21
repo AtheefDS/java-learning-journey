7d4813f0016bbfb5271622f1ba5c813349d92aa1
AtheefDS <atheefcse@gmail.com>
2026-06-21 23:39:41 +0530

    first commit

--- array.java
+++ array.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //How to get integer input in arrays and also using for loop
 import java.lang.System;
 import java.util.Scanner;
@@ -24,3 +25,31 @@ public class array {
     }
 }
 
+=======
+//How to get integer input in arrays and also using for loop
+import java.lang.System;
+import java.util.Scanner;
+public class array {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //get array size from user 
+        System.out.println("Enter your array size:");
+        int n = scan.nextInt();
+        //create array
+        int[] arr = new int[n];
+        System.out.println("Enter " + n  + " elements:");
+        //using for loop for easy for get input
+        for(int i =0; i<n ; i++){
+            arr[i] = scan.nextInt();
+        }
+        //print using for loop
+        for(int i=0 ; i<n ; i++){
+            System.out.println("Position at "+ i + " : " + arr[i]);
+        }
+
+
+
+    }
+}
+
+>>>>>>> 3a0a5c9 (Implement array input and output functionality in the array class)
