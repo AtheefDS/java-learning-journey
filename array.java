8029deab1b48a3f83b2feffacc9f8991d0271444
AtheefDS <atheefcse@gmail.com>
2026-06-21 23:49:18 +0530

    Fix comment typo in array printing functionality

--- array.java
+++ array.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //How to get integer input in arrays and also using for loop
 import java.lang.System;
 import java.util.Scanner;
@@ -15,7 +14,7 @@ public class array {
         for(int i =0; i<n ; i++){
             arr[i] = scan.nextInt();
         }
-        //print using for loop printing
+        //print using for loop printin
         for(int i=0 ; i<n ; i++){
             System.out.println("Position at "+ i + " : " + arr[i]);
         }
@@ -25,31 +24,4 @@ public class array {
     }
 }
 
-=======
-//How to get integer input in arrays and also using for loop
-import java.lang.System;
-import java.util.Scanner;
-public class array {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        //get array size from user 
-        System.out.println("Enter your array size:");
-        int n = scan.nextInt();
-        //create array
-        int[] arr = new int[n];
-        System.out.println("Enter " + n  + " elements:");
-        //using for loop for easy for get input
-        for(int i =0; i<n ; i++){
-            arr[i] = scan.nextInt();
-        }
-        //print using for loop printing
-        for(int i=0 ; i<n ; i++){
-            System.out.println("Position at "+ i + " : " + arr[i]);
-        }
 
-
-
-    }
-}
-
->>>>>>> 3a0a5c9 (Implement array input and output functionality in the array class)
