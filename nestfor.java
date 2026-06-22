5940e1c8512898bc7643ac5524ac5fc905758d26
AtheefDS <atheefcse@gmail.com>
2026-06-22 22:40:02 +0530

    Fix nested loop condition in nestfor class to correctly print asterisks

--- nestfor.java
+++ nestfor.java
@@ -4,11 +4,11 @@ public class nestfor {
     public static void main(String[] args) {
         for(int i = 0; i<3 ; i++){
             
-            for(int j =0 ; j<3 ; j++){
+            for(int j =0 ; j<=i ; j++){
                  System.out.print("*");
                 
             }
-            System.out.println("*");
+            System.out.println();
            
         }
     }
