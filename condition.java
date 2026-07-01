574f34826ee5fff1a4ca3cad0c1bd3ca1ab9c6e3
AtheefDS <atheefcse@gmail.com>
2026-07-01 11:04:22 +0530

    Add alphabet class to check if a character is a vowel

--- condition.java
+++ condition.java
@@ -15,3 +15,33 @@ public class condition {
         }
     }
 }
+
+// check character alphabet or not 
+class alphabet{
+    public static void main(String[] args) {
+        Scanner q2 = new Scanner(System.in);
+        System.out.println("Enter you favourite character");
+        char cha = q2.next().charAt(0);
+        if(cha == 'a' || cha == 'A'){
+            System.out.println(cha + " is a alphabet character");
+        }
+        else if(cha == 'e' || cha == 'E'){
+            System.out.println(cha + " is a alphabet character");
+        }
+
+         else if(cha == 'i' || cha == 'I'){
+            System.out.println(cha + " is a alphabet character");
+        }
+
+         else if(cha == 'o' || cha == 'O'){
+            System.out.println(cha + " is a alphabet character");
+        }
+         else if(cha == 'u' || cha == 'U'){
+            System.out.println(cha + " is a alphabet character");
+        }
+        else{
+            System.out.println("Enter valid character");
+        }
+
+    }
+}
