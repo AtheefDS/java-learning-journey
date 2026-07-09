796da87ac82df926a86eeea2011a457f1514d241
AtheefDS <atheefcse@gmail.com>
2026-07-09 18:24:22 +0800

    Add ex1 class to implement a simple number printing loop

---
+++ loop.java
@@ -0,0 +1,13 @@
+import java.lang.System;
+import java.util.Scanner;
+class ex1{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your number:");
+        int n = scan.nextInt();
+        for(int i = 1 ; i<n ; i++){
+            System.out.println(i);
+        }
+
+    }
+}
