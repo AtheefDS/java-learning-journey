e7732803af8399db9e4c90ddcabb9b377709e225
AtheefDS <atheefcse@gmail.com>
2026-07-09 18:47:56 +0800

    Add ex3 class to implement a number printing loop from 0 to n

--- loop.java
+++ loop.java
@@ -23,3 +23,15 @@ class ex2{
         }
     }
 }
+
+class ex3{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your number:");
+        int n = scan.nextInt();
+        int i ,j =0;
+        for(i=0;i<=n;i++){
+            System.out.println(i);
+        }
+    }
+}
