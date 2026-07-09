c3c81fe65d31159c7c9902beff83e9abe519d776
AtheefDS <atheefcse@gmail.com>
2026-07-09 18:34:19 +0800

    Add ex2 class to implement a number printing loop with inclusive range

--- loop.java
+++ loop.java
@@ -11,3 +11,15 @@ class ex1{
 
     }
 }
+
+class ex2{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your number");
+        int n = scan.nextInt();
+        int i = 1;
+        for(;i<=n;i++){
+            System.out.println(i);
+        }
+    }
+}
